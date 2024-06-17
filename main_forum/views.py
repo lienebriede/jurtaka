29cdd031acb7e9b816e16e6da4e2347803340e43
Liene Briede <lienebriede84@gmail.com>
2024-06-17 16:51:42 +0000

    Add pagination to post list in views and template

--- main_forum/views.py
+++ main_forum/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator
 
 def post_list(request):
     """
     Display all the posts (latest first)
     """
     queryset = Post.objects.all().order_by('-created_on')
+
+    paginator = Paginator(queryset, 5) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    is_paginated = page_obj.has_other_pages()
+
     context = {
-        'posts': queryset
+        'posts': queryset,
+        'page_obj': page_obj,
+        'posts': page_obj.object_list,
+        'is_paginated': is_paginated,
     }
 
     return render(request, "main_forum/index.html", context)
