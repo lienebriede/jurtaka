61dff6541cb2c6c6c518f7b60519471e25e30ef9
Liene Briede <lienebriede84@gmail.com>
2024-06-17 13:38:42 +0000

    Create and register models: post, comment, like

--- main_forum/models.py
+++ main_forum/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Variable for post status
+STATUS = ((0, "Pending"), (1, "Approved"), (2, "Deleted") )
+
+class Post(models.Model):
+    """
+    Model for posts
+    """
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts"
+        )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    post_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class Like(models.Model):
+    """
+    Model for likes
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Like  by '{self.user}' on '{self.post}'"
+        
+
+class Comment(models.Model):
+    """
+    Model for comments
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    comment_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
+
