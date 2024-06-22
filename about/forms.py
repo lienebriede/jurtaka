16467b7e97814d5425aad43b6cf0cfd42a8a50f1
Liene Briede <lienebriede84@gmail.com>
2024-06-22 19:15:26 +0000

    Style contact form

--- about/forms.py
+++ about/forms.py
@@ -5,3 +5,20 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['user', 'email', 'message']
+        widgets = {
+            'user': forms.TextInput(attrs={
+                'class': "form-control",
+            }),
+            'email': forms.TextInput(attrs={
+                'class': "form-control",
+            }),
+            'message': forms.Textarea(attrs={
+                'class': "form-control",
+                'style': 'height: 200px;',
+            }),
+        }
+        labels = {
+            'user': 'Your username:',
+            'email': 'Your e-mail:',
+            'message': 'Write your message here:',
+        }
